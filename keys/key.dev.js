cf5c6b59cc9fcc3e38e33c350fc1f7c7d300381e
Yevhen Volontyr <soapforwork@gmail.com>
2022-06-22 12:55:03 +0300

    +keys for dev and prod

---
+++ keys/key.dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+	MONGO_DB_URI: `mongodb://127.0.0.1:27017/together`,
+	SESSION_SECRET: `keyboard cat`,
+}
---
+++ keys/key.prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+	MONGO_DB_URI: process.env.MONGO_DB_URI,
+	SESSION_SECRET: process.env.SESSION_SECRET,
+}
